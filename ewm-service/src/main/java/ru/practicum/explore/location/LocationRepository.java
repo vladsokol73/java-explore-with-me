f5c59af996b1458034cb95f3294d1334d457da94
vladsokol73 <83404922+vladsokol73@users.noreply.github.com>
2023-02-27 21:42:40 +0400

    Delete ewm-service directory

--- ewm-service/src/main/java/ru/practicum/explore/location/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.explore.location;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationRepository extends JpaRepository<Location, Long> {
-
-}
