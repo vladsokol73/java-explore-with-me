871b6a5987ee23ea4c138f42a6c4eca929ba35f5
vladsokol73 <83404922+vladsokol73@users.noreply.github.com>
2023-02-27 21:34:15 +0400

    Update EndpointHitController.java

--- stats/stats-server/src/main/java/ru/practicum/explore/hits/EndpointHitController.java
+++ stats/stats-server/src/main/java/ru/practicum/explore/hits/EndpointHitController.java
@@ -1,7 +1,6 @@
 package ru.practicum.explore.hits;
 
 import lombok.RequiredArgsConstructor;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
