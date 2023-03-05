55493c200726ee36568104baedabf9c1028363ff
vladsokol73 <83404922+vladsokol73@users.noreply.github.com>
2023-03-05 13:23:47 +0400

    Delete ewm-service directory

--- ewm-service/src/main/java/ru/practicum/explore/category/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.explore.category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
--- ewm-service/src/main/java/ru/practicum/explore/request/RequestStatus.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.explore.request;
-
-public enum RequestStatus {
-    PENDING,
-    CONFIRMED,
-    REJECTED,
-    CANCELED
-}
